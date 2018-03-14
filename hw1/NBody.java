c0da91648335747ea5cc1b532033b9239a55cdff
FreecssX <31973984+FreecssX@users.noreply.github.com>
2018-03-14 16:48:38 +0800

    print out the information of the planets when the simulation stops

--- hw1/NBody.java
+++ hw1/NBody.java
@@ -22,7 +22,7 @@ public class NBody {
 		
 		/* ready to show the solar system */
 
-		StdAudio.loop("audio/2001.mid"); //play sound;
+		StdAudio.play("audio/2001.mid"); //play sound;
 		while (time < T) {
 			
 			/* update all the planets */
@@ -44,7 +44,14 @@ public class NBody {
 			time += dt;
 		}
 		StdAudio.close(); //close the sound;
+		StdOut.printf("%d\n", planetNum);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < planetNum; i++) {
+    		StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+    			planets[i].x, planets[i].y, planets[i].xVelocity, planets[i].yVelocity, planets[i].mass, planets[i].img);
+}
 	}
+
 	public static Planet getPlanet(In in) {
 		String line = in.readLine();
 		double x = Double.parseDouble(in.readString());
