8ded577cf28bca549275bbccd3f3cf7cc478bfd2
FreecssX <31973984+FreecssX@users.noreply.github.com>
2018-04-14 17:29:51 +0800

    Complete Username and UsernameBank

--- hw7/Username.java
+++ hw7/Username.java
@@ -1,28 +1,97 @@
+import java.util.Random;
 public class Username {
 
     // Potentially useless note: (int) '0' == 48, (int) 'a' == 97
 
     // Instance Variables (remember, they should be private!)
     // YOUR CODE HERE
-
+	private String Username;
     public Username() {
-        // YOUR CODE HERE
+        Random rand = new Random(); 
+        int value1 = rand.nextInt(26) + 97; 
+        int value2 = rand.nextInt(26) + 97;
+        int value3 = rand.nextInt(26) + 97;
+        boolean Cap1 = rand.nextInt(2) == 1;
+        boolean Cap2 = rand.nextInt(2) == 1;
+        boolean Cap3 = rand.nextInt(2) == 1;
+        int num = rand.nextInt(2) + 2;
+       
+        char a = (char) value1;
+        if(Cap1) {
+        	a = Character.toUpperCase((a));
+        }
+        char b = (char) value2;
+        if(Cap2) {
+        	b = Character.toUpperCase(b);
+        }
+        char c = (char) value3;
+        if(Cap3) {
+        	c = Character.toUpperCase(c);
+        }
+        String result = Character.toString(a) + Character.toString(b);
+        if(num == 3) {
+        	result = result + Character.toString(c);
+        }
+        Username = result;
     }
 
     public Username(String reqName) {
-        // YOUR CODE HERE
+        if(reqName == null) {
+        	throw new NullPointerException("Requested username is null!");
+        }
+        if(reqName.length() != 2 || reqName.length() != 3) {
+        	throw new IllegalArgumentException("wrong length");
+        } else {
+        	for(int i = 0; i < reqName.length(); i += 1) {
+        		char s = reqName.charAt(i);
+        		int k = (int) s;
+        		if((k <= 48 + 9 && k >= 48) || (k >= 97 && k <= 97 + 25)
+        		    || (k >= ((int) 'A') && k <= ((int) 'A' + 25))) {
+        			
+        		} else {
+        			throw new IllegalArgumentException("wrong characters");
+        		}
+        	}
+        	Username = reqName;
+        }
     }
-
+    private boolean charEquals(char a, char b) {
+    	return a == b || Math.abs(((int) a - (int) b))== Math.abs(((int) 'a' - (int) 'b'));
+    }
     @Override
     public boolean equals(Object o) {
-        // YOUR CODE HERE
+        if(o instanceof Username) {
+        	if(((String) o).length() == Username.length() ) {
+        		for(int i = 0; i < Username.length(); i += 1) {
+        			if(!(charEquals(Username.charAt(i), ((String) o).charAt(i)))) {
+        				break;
+        			}
+        			return true;
+        		}
+        	}
+        	else return false;
+        }
         return false;
     }
 
     @Override
     public int hashCode() { 
-        // YOUR CODE HERE
-        return 0;
+        String s = "";
+        int result = 0;
+        for(int i = 0; i < Username.length(); i += 1) {
+        	s = s + Character.toString(Character.toLowerCase(Username.charAt(i)));
+        }
+        for(int i = 0; i < s.length(); i += 1) {
+        	char k = Character.toLowerCase(Username.charAt(i));
+        	int num = (int) k;
+        	if(num <=57) num = num - 48;
+        	else num = num - 97;
+        	result = result * 36 + num;
+        }
+        return result;
+    }
+    public String getName() {
+    	return Username;
     }
 
     public static void main(String[] args) {
--- hw7/UsernameBank.java
+++ hw7/UsernameBank.java
@@ -1,56 +1,105 @@
 import java.util.Map;
-
+import java.util.HashMap;
 public class UsernameBank {
 
     // Instance variables (remember, they should be private!)
-    // YOUR CODE HERE
-
+    private HashMap<Username, String> ub;
+    private HashMap<String, Integer> badU;
+    private HashMap<String, Integer> badE;
     public UsernameBank() {
-        // YOUR CODE HERE
+        ub = new HashMap<Username, String>();
+        badU = new HashMap<String, Integer>();
+        badE = new HashMap<String, Integer>();
     }
-
+    private boolean isValidEmail(String email) {
+        int numOfat = 0;
+        if(email == null) return false;
+        for(int i = 0; i < email.length(); i += 1) {
+            if(email.charAt(i) == '@' ) {
+                numOfat += 1;
+            }
+        }
+        if(numOfat < 1) return false;
+        return true;
+    }
     public void generateUsername(String username, String email) {
-        // YOUR CODE HERE
+        try {
+            Username un = new Username(username);
+            String e = ub.get(un);
+            if(e != null) {
+                System.out.println("IllegalArgument");
+                return;
+            }
+            if(isValidEmail(email)) {
+                ub.put(un, email);
+            } else {
+                System.out.println("Invalid email address");
+            }
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
     }
 
     public String getEmail(String username) {
-        // YOUR CODE HERE
-        return null;
+        if(username == null) {
+            throw new NullPointerException();
+        }
+        try {
+            Username un = new Username(username);
+            String email = ub.get(un);
+            return email;
+        }
+        catch(Exception e) {
+            Integer num = badU.get(username);
+            if(num == null) badU.put(username, 1);
+            else badU.put(username, num + 1);
+            return null;
+        }
     }
 
     public String getUsername(String userEmail)  {
-        // YOUR CODE HERE
+        if(userEmail == null) throw new NullPointerException();
+        for(Username un: ub.keySet()) {
+            String email = ub.get(un);
+            if(email == null) continue;
+            if(ub.get(ub).equals(userEmail)) return un.getName();
+        } 
+        Integer num = badE.get(userEmail);
+        if(num == null) badE.put(userEmail, 1);
+        else badE.put(userEmail, num + 1); 
         return null;
     }
 
     public Map<String, Integer> getBadEmails() {
-        // YOUR CODE HERE
-        return null;
+        return badE;
     }
 
     public Map<String, Integer> getBadUsernames() {
-        // YOUR CODE HERE
-        return null;
+        return badU;
     }
 
     public String suggestUsername() {
-        // YOUR CODE HERE
+        for(int i = 0; i < 10; i += 1) {
+            Username un = new Username();
+            if(ub.get(un) == null) return un.getName();
+        }
         return null;
     }
 
     // The answer is somewhere in between 3 and 1000.
     public static final int followUp() {
         // YOUR CODE HERE
-        return 0;
+        return 3; // the total number of userNames should be 36^n where 36^n + 36^(n-1) + ... + 36^2 <= 2^16
     }
 
     // Optional, suggested method. Use or delete as you prefer.
-    private void recordBadUsername(String username) {
+    /*private void recordBadUsername(String username) {
         // YOUR CODE HERE
     }
 
     // Optional, suggested method. Use or delete as you prefer.
     private void recordBadEmail(String email) {
         // YOUR CODE HERE
-    }
+    }*/
 }
